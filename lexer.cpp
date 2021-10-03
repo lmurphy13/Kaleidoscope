9192c6af49d637b6ce2ecfa9647c6707a4413a77
Liam Murphy <liam@spock.attlocal.net>
2021-10-02 19:46:48 -0500

    initial commit

---
+++ lexer.cpp
@@ -0,0 +1,76 @@
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+// The lexer returns tokens [0-255] if it is an unknown character, otherwise
+// one of these for known things.
+enum Tokens {
+    tok_eof = -1,
+
+    // commands
+    tok_def = -2,
+    tok_extern = -3,
+
+    // primary
+    tok_identifier = -4,
+    tok_number = -5
+};
+
+static std::string IdentifierStr; // Filled in if tok_identifier
+static double NumVal;
+
+// gettok - Return the net token from standard input
+static int gettok() {
+    static int LastChar = ' ';
+
+    // Skip any whitespace
+    while (isspace(LastChar))
+        LastChar = getchar();
+
+    if (isalpha(LastChar)) {
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+
+        if (IdentifierStr == "def")
+            return tok_def;
+        if (IdentifierStr == "extern")
+            return tok_extern;
+        return tok_identifier;
+    }
+
+    if (isdigit(LastChar) || LastChar == '.') {
+        std::string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), nullptr);
+        return tok_number;
+    }
+
+    if (LastChar == '#') {
+        // comment until end of line
+        do
+            LastChar = getchar();
+        while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF)
+            return gettok();
+    }
+
+    // Check for end of file. Don't consume EOF
+    if (LastChar == EOF)
+        return tok_eof;
+
+    // Otherwise, just return the character as its ascii value
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+}
